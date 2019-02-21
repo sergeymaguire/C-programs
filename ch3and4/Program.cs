56535881464c96c5a5c00c21c2b8e879b5efc505
sergeymaguire <sergeymaguire@gmail.com>
2019-02-21 11:29:51 -0800

    Messing around with number types and strings

--- ch3and4/Program.cs
+++ ch3and4/Program.cs
@@ -10,7 +10,27 @@ namespace ch3and4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            //int aLotofMoney = 23548797;
+            //int aLotofMoneys = 24_4578_45;
+            //int annualSalary = -70000;
+            //Console.WriteLine("test");
+            //Console.WriteLine(aLotofMoney);
+            //Console.WriteLine(aLotofMoneys);
+            //  Console.WriteLine(annualSalary);
+            int anInt = 45555557;
+            uint aUnsnInt = 257;
+            double myMoney = 14.00;
+            Console.WriteLine(myMoney);
+            double someMoney = 123;
+            string moneyString;
+            moneyString = someMoney.ToString("F4");
+            Console.WriteLine(moneyString);
+            Console.WriteLine("Here is an int {0} here is the unassinged int {1}.", anInt, aUnsnInt);
+            double mohhhMoney = 478952;
+            string currencyMoney;
+            currencyMoney = mohhhMoney.ToString("C");
+            Console.WriteLine(currencyMoney);
+            Console.ReadLine();
         }
     }
 }
